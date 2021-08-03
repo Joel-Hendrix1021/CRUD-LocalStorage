4ebb9429d5a399d21c53ec67692639be0ef9f556
Joel Pimentel <joelpimentel1995@gmail.com>
2021-08-03 09:16:50 -0500

    change html

--- app.js
+++ app.js
@@ -1,33 +1,73 @@
-const $form = document.getElementById('form')
-const $tbody = document.querySelector('tbody')
-const $template = document.getElementById('template')
 
-window.addEventListener('DOMContentLoaded', () => {
-    renderTasks()
-});
+const renderForm=()=>{
+    document.querySelector('body').innerHTML = `
+    <h1 style="text-align: center;">tasks</h1>
+    <div class="container-sm">
+        <form id="form" >
+            <div class="mb-3">
+                <input type="text" name="task" required autofocus class="form-control" placeholder="tasks">
+            </div>
+            <div class="mb-3">
+                <textarea class="form-control" name="description" required placeholder="task description" rows="3"></textarea>
+            </div>
+            <div class="d-grid gap-2">
+                <button class="btn btn-success" type="submit">Aceptar</button>
+            </div>
+        </form>
+    </div>
 
-$form.addEventListener('submit', (e)=>{
-    e.preventDefault() 
-    if(e.target[2].dataset.id){
-        updateTask(e.target)
-    }else {
-        let tasks =[{
-            id: getRandom(),
-            name : e.target.task.value,
-            description: e.target.description.value
-        }]
-    
-        localStorage.setItem(tasks[0].id, JSON.stringify(tasks))
-    }
+    <table class="table table mt-4" style="color: #61DAFB;">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Task</th>
+        <th scope="col">Description</th>
+        <th scope="col">Action</th>
+      </tr>
+    </thead>
+    <tbody> 
+
+    </tbody>
+  </table>
    
-    renderTasks()
-    e.target.reset()
-})
+    `
+    $tbody = document.querySelector('tbody')
+}
+
+
+let $form 
+let $tbody 
+let $template 
+
+const formEvent=()=>{
+    $form = document.getElementById('form')
+    $form.addEventListener('submit', (e)=>{
+        e.preventDefault() 
+        if(e.target[2].dataset.id){
+            updateTask(e.target)
+        }else {
+            let tasks =[{
+                id: getRandom(),
+                name : e.target.task.value,
+                description: e.target.description.value
+            }]
+        
+            localStorage.setItem(tasks[0].id, JSON.stringify(tasks))
+        }
+       
+        renderTasks()
+        e.target.reset()
+    })
+    
+}
+
 
 const renderTasks=()=>{
+
     $tbody.innerHTML = ''
     const $frag = document.createDocumentFragment()
     const tasks = Object.keys(localStorage)
+    console.log(tasks)
     const $td = $template.content.querySelectorAll('td')
     const $th = $template.content.querySelectorAll('th')
      tasks.forEach((el, index) => {  
@@ -47,7 +87,11 @@ const getRandom=()=>{
     return random
 }
 
-$tbody.addEventListener('click', (e)=>{
+    
+$template = document.getElementById('template')
+
+document.addEventListener('click', (e)=>{
+  
    if(e.target.id == 'delete'){
       const id =  parseFloat(e.target.parentElement.dataset.id)
        localStorage.removeItem(id)
@@ -76,3 +120,10 @@ const updateTask=(n)=>{
 }
 
 
+window.addEventListener('DOMContentLoaded', (event) => {
+     
+    renderForm()
+    renderTasks()
+    formEvent()
+  
+});
